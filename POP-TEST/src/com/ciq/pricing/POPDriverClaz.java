242c877b770fcffde3fa955eef4caa8cb671db5d
ssaindavena <ssaindavena@spcapitaliq.com>
2014-06-11 20:16:13 +0530

    initial commit of pop project

---
+++ POP-TEST/src/com/ciq/pricing/POPDriverClaz.java
@@ -0,0 +1,12 @@
+package com.ciq.pricing;
+
+public class POPDriverClaz {
+
+	public static void main(String[] args) {
+		
+		
+		POPProjectClaz pop = new POPProjectClaz();
+		
+		System.out.println(pop.getProjectInfo());
+	}
+}
---
+++ POP-TEST/src/com/ciq/pricing/POPProjectClaz.java
@@ -0,0 +1,19 @@
+package com.ciq.pricing;
+
+public class POPProjectClaz {
+	
+	public String getProjectInfo(){
+		
+		
+		String pcdbInfo = getPcdbInfo();
+		
+		return pcdbInfo;
+	}
+
+	private String getPcdbInfo() {
+		
+		
+		return "this is pcdb project";
+	}
+
+}
